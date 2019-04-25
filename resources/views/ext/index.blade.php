b145b783ffcdfb37953f630852b6e62f4008dd83
王荣 <never615@gmail.com>
2019-04-25 19:52:43 +0800

    Update index.blade.php
    点击菜单生成的tab中的id使用url的绝对路径,调用的js方法依赖字laravel-admin-enhance

--- resources/views/ext/index.blade.php
+++ resources/views/ext/index.blade.php
@@ -1,145 +1,148 @@
 @extends('admin::index', ['header' => $header])
 
 @section('content')
-<div class="content-iframe">
-    <div class="tab-content " id="tab-content">
+    <div class="content-iframe">
+        <div class="tab-content " id="tab-content">
+        </div>
     </div>
-</div>
 
-<script>
-    $(function() {
-        window.refresh_current = "{{ $trans['refresh_current'] }}";
-        window.open_in_new = "{{ $trans['open_in_new'] }}";
-        window.open_in_pop = "{{ $trans['open_in_pop'] }}";
+    <script>
+        $(function () {
+            window.refresh_current = "{{ $trans['refresh_current'] }}";
+            window.open_in_new = "{{ $trans['open_in_new'] }}";
+            window.open_in_pop = "{{ $trans['open_in_pop'] }}";
 
-        window.use_icon = "{{ $use_icon }}" == '1';
-        window.pass_urls = '{{ $pass_urls }}'.split(',');
-        window.home_title = '{{ $home_title }}';
-        window.home_uri = '{{ $home_uri }}';
-        window.home_icon = '{{ $home_icon }}';
-        window.iframes_index = '{{ $iframes_index }}';
-        window.tabs_left = '{{ $tabs_left }}';
+            window.use_icon = "{{ $use_icon }}" == '1';
+            window.pass_urls = '{{ $pass_urls }}'.split(',');
+            window.home_title = '{{ $home_title }}';
+            window.home_uri = '{{ $home_uri }}';
+            window.home_icon = '{{ $home_icon }}';
+            window.iframes_index = '{{ $iframes_index }}';
+            window.tabs_left = '{{ $tabs_left }}';
 
-        window.openPop = function(url, title) {
-            layer.open({
-                type: 2,
-                title: title,
-                anim: 2,
-                closeBtn: 1,
-                shade: false,
-                maxmin: true, //开启最大化最小化按钮
-                area: [$('#tab-content').width() + 'px', ($('#tab-content').height() - 5) + 'px'],
-                content: url,
-                offset: 'rb'
-            });
-        }
-
-        window.openTab = function(url, title, icon, page_id, close, urlType) {
-            if (!url) {
-                alert('url is empty.');
-                return;
+            window.openPop = function (url, title) {
+                layer.open({
+                    type: 2,
+                    title: title,
+                    anim: 2,
+                    closeBtn: 1,
+                    shade: false,
+                    maxmin: true, //开启最大化最小化按钮
+                    area: [$('#tab-content').width() + 'px', ($('#tab-content').height() - 5) + 'px'],
+                    content: url,
+                    offset: 'rb'
+                });
             }
-            addTabs({
-                id: page_id || url.replace(/\W/g, '_'),
-                title: title || 'New page',
-                close: close != false && close != 0,
-                url: url,
-                urlType: urlType || 'absolute',
-                icon: '<i class="fa ' + (icon || 'fa-file-text') + '"></i>'
-            });
-        }
 
-        if (!window.layer) {
-            window.layer = {
-                load: function() {
-                    var html = '<div style="z-index:999;margin:0 auto;position:fixed;top:90px;left:50%;" class="loading-message"><img src="/vendor/laravel-admin-ext/iframe-tabs/images/loading-spinner-grey.gif" /></div>';
-                    $('.tab-content').append(html);
-                    return 1;
-                },
-                close: function(index) {
-                    $('.tab-content .loading-message').remove();
-                },
-                open: function() {
-                    alert('layer.js dose not work.');
+            window.openTab = function (url, title, icon, page_id, close, urlType) {
+                if (!url) {
+                    alert('url is empty.');
+                    return;
                 }
-            };
-        }
-
-        $('body').on('click', '.sidebar-menu li a,.navbar-nav>li a,.sidebar .user-panel a', function() {
-            var url = $(this).attr('href');
-            if (!url || url == '#' || /^javascript|\(|\)/i.test(url)) {
-                return;
+                addTabs({
+                    id: page_id || url.replace(/\W/g, '_'),
+                    title: title || 'New page',
+                    close: close != false && close != 0,
+                    url: url,
+                    urlType: urlType || 'absolute',
+                    icon: '<i class="fa ' + (icon || 'fa-file-text') + '"></i>'
+                });
             }
 
-            if ($(this).attr('target') == '_blank') {
-                return;
+            if (!window.layer) {
+                window.layer = {
+                    load: function () {
+                        var html = '<div style="z-index:999;margin:0 auto;position:fixed;top:90px;left:50%;" class="loading-message"><img src="/vendor/laravel-admin-ext/iframe-tabs/images/loading-spinner-grey.gif" /></div>';
+                        $('.tab-content').append(html);
+                        return 1;
+                    },
+                    close: function (index) {
+                        $('.tab-content .loading-message').remove();
+                    },
+                    open: function () {
+                        alert('layer.js dose not work.');
+                    }
+                };
             }
 
-            if ($(this).hasClass('iframes-pass-url')) {
-                return;
-            }
+            $('body').on('click', '.sidebar-menu li a,.navbar-nav>li a,.sidebar .user-panel a', function () {
+                var url = $(this).attr('href');
+                if (!url || url == '#' || /^javascript|\(|\)/i.test(url)) {
+                    return;
+                }
 
-            if (window.pass_urls) {
-                for (var i in window.pass_urls) {
-                    if (url.indexOf(window.pass_urls[i]) > -1) {
-                        return true;
+                if ($(this).attr('target') == '_blank') {
+                    return;
+                }
+
+                if ($(this).hasClass('iframes-pass-url')) {
+                    return;
+                }
+
+                if (window.pass_urls) {
+                    for (var i in window.pass_urls) {
+                        if (url.indexOf(window.pass_urls[i]) > -1) {
+                            return true;
+                        }
                     }
                 }
-            }
-            var icon = '<i class="fa fa-file-text"></i>';
-            if ($(this).find('i.fa').size()) {
-                var icon = $(this).find('i.fa').prop("outerHTML");
-            }
-            var span = $(this).find('span');
-            addTabs({
-                id: url.replace(/\W/g, '_'),
-                title: span.size() ? span.text() : $(this).text().length ? $(this).text() : '*',
-                close: true,
-                url: url,
-                urlType: 'absolute',
-                icon: icon
+                var icon = '<i class="fa fa-file-text"></i>';
+                if ($(this).find('i.fa').size()) {
+                    var icon = $(this).find('i.fa').prop("outerHTML");
+                }
+                var span = $(this).find('span');
+
+                url = etUrlRelativePath(url);
+
+                addTabs({
+                    id: url.replace(/\W/g, '_'),
+                    title: span.size() ? span.text() : $(this).text().length ? $(this).text() : '*',
+                    close: true,
+                    url: url,
+                    urlType: 'absolute',
+                    icon: icon
+                });
+
+                if ($(this).parents('.dropdown').size()) {
+                    $(this).parents('.dropdown').find('.dropdown-toggle').trigger('click');
+                }
+                return false;
             });
 
-            if ($(this).parents('.dropdown').size()) {
-                $(this).parents('.dropdown').find('.dropdown-toggle').trigger('click');
+            if (window == top) {
+                addTabs({
+                    id: '_admin_dashboard',
+                    title: window.home_title,
+                    close: false,
+                    url: window.home_uri,
+                    urlType: 'absolute',
+                    icon: '<i class="fa ' + window.home_icon + '"></i>'
+                });
+            } else {
+                if (location.href == window.iframes_index) {
+                    $('body').html('....');
+                    location.href = window.home_uri;
+                }
             }
-            return false;
-        });
 
-        if (window == top) {
-//            addTabs({
-//                id: '_admin_dashboard',
-//                title: window.home_title,
-//                close: false,
-//                url: window.home_uri,
-//                urlType: 'absolute',
-//                icon: '<i class="fa ' + window.home_icon + '"></i>'
-//            });
-        } else {
-            if (location.href == window.iframes_index) {
-                $('body').html('....');
-                location.href = window.home_uri;
+            $('body').on('click', '.main-header a.logo', function () {
+                return false;
+            });
+
+            $('.navbar-custom-menu').css('background-color', $('.main-header .navbar').css('background-color'));
+
+            $('.navbar-custom-menu').show(); // delete it in future
+
+            if (!$(".navbar-custom-menu>ul>*:first").hasClass('tab-options')) {
+                $(".navbar-custom-menu>ul>*:first").before($('.navbar-custom-menu>ul>li.tab-options'));
             }
-        }
 
-        $('body').on('click', '.main-header a.logo', function() {
-            return false;
+            $('.content-tabs').css({
+                'left': window.tabs_left + 'px',
+                'width': '100%'
+            });
+
         });
-
-        $('.navbar-custom-menu').css('background-color', $('.main-header .navbar').css('background-color'));
-
-        $('.navbar-custom-menu').show(); // delete it in future
-
-        if (!$(".navbar-custom-menu>ul>*:first").hasClass('tab-options')) {
-            $(".navbar-custom-menu>ul>*:first").before($('.navbar-custom-menu>ul>li.tab-options'));
-        }
-
-        $('.content-tabs').css({
-            'left': window.tabs_left + 'px',
-            'width': '100%'
-        });
-
-    });
-</script>
+    </script>
 
 @endsection
